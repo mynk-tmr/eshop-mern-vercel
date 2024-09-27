d8defea312c226cb2692f8c52fe797a7792bdd36
Mayank Tomar <146926037+mynk-tmr@users.noreply.github.com>
2024-09-27 21:01:51 +0530

    add Rubik font as base font-face

--- apps/client/src/app.tsx
+++ apps/client/src/app.tsx
@@ -1,37 +1,11 @@
 import "@/styles/base.css";
-import { Avatar } from "@/components/atoms/avatar";
-import { IconButton } from "@/components/atoms/buttons";
-import { BellIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import "@fontsource-variable/rubik";
+import { Header } from "./components/organisms/header";
 
 export const App = () => {
 	return (
-		<header className="flex items-center justify-between *:flex *:gap-2 *:items-center">
-			<div>
-				<Avatar
-					src="https://i.pravatar.cc/150"
-					alt="user-avatar"
-					fallback="M"
-				/>
-				<span className="text-sm">
-					Welcome back <br />
-					<strong>Mayank</strong>
-				</span>
-			</div>
-			<div>
-				<IconButton>
-					<BellIcon className="size-5" />
-				</IconButton>
-				<ThemeButton />
-			</div>
-		</header>
-	);
-};
-
-const ThemeButton = () => {
-	return (
-		<IconButton>
-			<SunIcon className="size-5" />
-			{/* <MoonIcon className="size-5" /> */}
-		</IconButton>
+		<>
+			<Header />
+		</>
 	);
 };
---
+++ apps/client/src/components/organisms/header.tsx
@@ -0,0 +1,36 @@
+import { Avatar } from "@/components/atoms/avatar";
+import { IconButton } from "@/components/atoms/buttons";
+import { BellIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
+
+export const Header = () => {
+	return (
+		<header className="flex items-center justify-between *:flex *:gap-2 *:items-center">
+			<div>
+				<Avatar
+					src="https://i.pravatar.cc/150"
+					alt="user-avatar"
+					fallback="M"
+				/>
+				<span className="text-sm">
+					Welcome back <br />
+					<strong>Mayank</strong>
+				</span>
+			</div>
+			<div>
+				<IconButton>
+					<BellIcon className="size-5" />
+				</IconButton>
+				<ThemeButton />
+			</div>
+		</header>
+	);
+};
+
+const ThemeButton = () => {
+	return (
+		<IconButton>
+			<SunIcon className="size-5" />
+			{/* <MoonIcon className="size-5" /> */}
+		</IconButton>
+	);
+};
