a5f94ff8a2fb57d78f01b295a8bc1b07ef750adf
Mayank Tomar <146926037+mynk-tmr@users.noreply.github.com>
2024-09-28 03:02:00 +0530

    build(client): add new library to ease css var integrations

--- apps/client/src/components/atoms/others.tsx
+++ apps/client/src/components/atoms/others.tsx
@@ -2,4 +2,4 @@ import { twx } from "@/lib/utils";
 
 export const Dot = twx.span`flex size-2 rounded-full bg-sky-500 translate-y-1`;
 
-export const Card = twx.article`p-4 rounded-xl border text-card-foreground shadow`;
+export const Card = twx.article`p-4 rounded-xl border border-card text-card-foreground shadow`;
--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,65 +1,124 @@
 import plugin from "tailwindcss/plugin";
+const themeSwapper = require("tailwindcss-theme-swapper");
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ["class"],
 	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./index.html"],
 	theme: {
-		extend: {
-			borderRadius: {
-				lg: "var(--radius)",
-				md: "calc(var(--radius) - 2px)",
-				sm: "calc(var(--radius) - 4px)",
-			},
-			colors: {
-				attention: "hsl(var(--attention))",
-				background: "hsl(var(--background))",
-				foreground: "hsl(var(--foreground))",
-				card: {
-					DEFAULT: "hsl(var(--card))",
-					foreground: "hsl(var(--card-foreground))",
-				},
-				popover: {
-					DEFAULT: "hsl(var(--popover))",
-					foreground: "hsl(var(--popover-foreground))",
-				},
-				primary: {
-					DEFAULT: "hsl(var(--primary))",
-					foreground: "hsl(var(--primary-foreground))",
-				},
-				secondary: {
-					DEFAULT: "hsl(var(--secondary))",
-					foreground: "hsl(var(--secondary-foreground))",
-				},
-				muted: {
-					DEFAULT: "hsl(var(--muted))",
-					foreground: "hsl(var(--muted-foreground))",
-				},
-				accent: {
-					DEFAULT: "hsl(var(--accent))",
-					foreground: "hsl(var(--accent-foreground))",
-				},
-				destructive: {
-					DEFAULT: "hsl(var(--destructive))",
-					foreground: "hsl(var(--destructive-foreground))",
-				},
-				border: "hsl(var(--border))",
-				input: "hsl(var(--input))",
-				ring: "hsl(var(--ring))",
-				chart: {
-					1: "hsl(var(--chart-1))",
-					2: "hsl(var(--chart-2))",
-					3: "hsl(var(--chart-3))",
-					4: "hsl(var(--chart-4))",
-					5: "hsl(var(--chart-5))",
-				},
-			},
-		},
+		extend: {},
 	},
 	plugins: [
 		require("tailwindcss-animate"),
 		plugin(({ addVariant }) => {
 			addVariant("hocus", ["&:hover", "&:focus-visible", "&:active"]);
+		}),
+		themeSwapper({
+			themes: [
+				{
+					name: "base",
+					selectors: [":root"],
+					theme: {
+						borderRadius: {
+							lg: "0.5rem",
+							md: "calc(0.5rem - 2px)",
+							sm: "calc(0.5rem - 4px)",
+						},
+						colors: {
+							attention: "hsl(11 91% 35%)",
+							background: "hsl(239 11% 95%)",
+							foreground: "hsl(239 5% 10%)",
+							card: {
+								DEFAULT: "hsl(239 11% 90%)",
+								foreground: "hsl(239 5% 15%)",
+							},
+							popover: {
+								DEFAULT: "hsl(239 11% 95%)",
+								foreground: "hsl(239 95% 10%)",
+							},
+							primary: {
+								DEFAULT: "hsl(239 82% 56%)",
+								foreground: "hsl(0 0% 100%)",
+							},
+							secondary: {
+								DEFAULT: "hsl(239 11% 85%)",
+								foreground: "hsl(0 0% 0%)",
+							},
+							muted: {
+								DEFAULT: "hsl(201 11% 85%)",
+								foreground: "hsl(239 5% 40%)",
+							},
+							accent: {
+								DEFAULT: "hsl(211 12% 48%)",
+								foreground: "hsl(239 5% 15%)",
+							},
+							destructive: {
+								DEFAULT: "hsl(0 50% 50%)",
+								foreground: "hsl(239 5% 90%)",
+							},
+							border: "hsl(239 20% 50%)",
+							input: "hsl(239 20% 50%)",
+							ring: "hsl(239 82% 56%)",
+							chart: {
+								1: "hsl(12 76% 61%)",
+								2: "hsl(173 58% 39%)",
+								3: "hsl(197 37% 24%)",
+								4: "hsl(43 74% 66%)",
+								5: "hsl(27 87% 67%)",
+							},
+						},
+					},
+				},
+				{
+					name: "dark",
+					selectors: [".dark"],
+					theme: {
+						colors: {
+							attention: "hsl(50 98% 64%)",
+							background: "hsl(239 11% 10%)",
+							foreground: "hsl(239 5% 90%)",
+							card: {
+								DEFAULT: "hsl(239 11% 85%)",
+								foreground: "hsl(239 5% 90%)",
+							},
+							popover: {
+								DEFAULT: "hsl(239 11% 5%)",
+								foreground: "hsl(239 5% 90%)",
+							},
+							primary: {
+								DEFAULT: "hsl(239 82% 56%)",
+								foreground: "hsl(0 0% 100%)",
+							},
+							secondary: {
+								DEFAULT: "hsl(239 11% 20%)",
+								foreground: "hsl(0 0% 100%)",
+							},
+							muted: {
+								DEFAULT: "hsl(201 11% 25%)",
+								foreground: "hsl(239 5% 65%)",
+							},
+							accent: {
+								DEFAULT: "hsl(240, 3%, 94%)",
+								foreground: "hsl(239 5% 90%)",
+							},
+							destructive: {
+								DEFAULT: "hsl(0 50% 50%)",
+								foreground: "hsl(239 5% 90%)",
+							},
+							border: "hsl(239 20% 50%)",
+							input: "hsl(239 20% 50%)",
+							ring: "hsl(239 82% 56%)",
+							chart: {
+								1: "hsl(220 70% 50%)",
+								2: "hsl(160 60% 45%)",
+								3: "hsl(30 80% 55%)",
+								4: "hsl(280 65% 60%)",
+								5: "hsl(340 75% 55%)",
+							},
+						},
+					},
+				},
+			],
 		}),
 	],
 };
